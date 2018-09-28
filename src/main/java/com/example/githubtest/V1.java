6637a57822731b7287c73dacf9211ff76928287c
MoJianyue <1194229162@qq.com>
2018-09-28 15:25:40 +0800

    4

--- src/main/java/com/example/githubtest/V1.java
+++ src/main/java/com/example/githubtest/V1.java
@@ -4,5 +4,7 @@ package com.example.githubtest;
 * @version 创建时间：2018年9月28日 下午3:06:09
 */
 public class V1 {
-
+    public static void main(String[] args) {
+        System.out.println("dssdgadf");
+    }
 }
